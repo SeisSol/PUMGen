984029218d5fc282a847f95c6f59c7d7714fbb92
Thomas-Ulrich <thomas.ulrich@geophysik.uni-muenchen.de>
2021-06-28 14:08:42 +0200

    remove ACIS in FindSimmetrix and make parasolid optional

--- cmake/FindSimmetrix.cmake
+++ cmake/FindSimmetrix.cmake
@@ -15,12 +15,13 @@ find_library(APF_SIM_LIB apf_sim)
 
 set(SIM_LIB_HINT ${SIMMETRIX_ROOT}/lib/x64_rhel7_gcc48)
 
-find_library(SIM_ACIS_LIB NAMES SimAcis2019 SimAcis2020 PATHS ${SIM_LIB_HINT})
 find_library(SIM_DISCRETE_LIB SimDiscrete ${SIM_LIB_HINT})
 find_library(SIM_MESHING_LIB SimMeshing ${SIM_LIB_HINT})
 find_library(SIM_MESH_TOOLS_LIB SimMeshTools ${SIM_LIB_HINT})
 find_library(SIM_MODEL_LIB SimModel ${SIM_LIB_HINT})
+if (PARASOLID)
 find_library(SIM_PARASOLID_LIB NAMES SimParasolid310 SimParasolid320 PATHS ${SIM_LIB_HINT})
+endif()
 find_library(SIM_PARTITIONED_MESH_LIB SimPartitionedMesh ${SIM_LIB_HINT})
 find_library(SIM_PARTITIONED_MESH_MPI_LIB SimPartitionedMesh-mpi ${SIM_LIB_HINT})
 find_library(SIM_PARTITINED_WRAPPER_LIB SimPartitionWrapper-${SIM_MPI} ${SIM_LIB_HINT})
@@ -31,17 +32,21 @@ get_filename_component(SIM_PS_KRNL_LIB_DIR ${SIM_PS_KRNL_LIB} DIRECTORY)
 list(APPEND SIMMETRIX_LIBRARIES
   "${GMI_SIM_LIB}"
   "${APF_SIM_LIB}"
-  "${SIM_ACIS_LIB}"
   "${SIM_DISCRETE_LIB}"
   "${SIM_EXPORT_LIB}"
   "${SIM_MESHING_LIB}"
   "${SIM_MESH_TOOLS_LIB}"
-  "${SIM_PARASOLID_LIB}"
   "${SIM_PARTITIONED_MESH_MPI_LIB}"
   "${SIM_PARTITINED_WRAPPER_LIB}"
   "${SIM_MODEL_LIB}"
   "${SIM_PS_KRNL_LIB}"
 )
+
+if (PARASOLID)
+list(APPEND SIMMETRIX_LIBRARIES
+  "${SIM_PARASOLID_LIB}"
+)
+endif()
 
 string(REGEX REPLACE ".*/([0-9]+).[0-9]-.*" "\\1" SIM_MAJOR_VER ${SIM_MODEL_LIB})
 find_package_handle_standard_args(SIMMETRIX DEFAULT_MSG
