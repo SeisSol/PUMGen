62251d221ba683a7251a59ea0334888650d66d42
Lukas Krenz <lukas@krenz.land>
2021-09-02 11:30:58 +0200

    Add initial easi version

--- src/input/EasiMeshSize.h
+++ src/input/EasiMeshSize.h
@@ -2,21 +2,30 @@
 #define PUMGEN_EASIMESHSIZE_H
 
 #include <array>
-#include <string_view>
+#include <memory>
+#include <string>
+#include "easi/Component.h"
+
+// Workaround for easi linking bug
+namespace easi {
+class YAMLParser;
+}
+struct ElasticMaterial {
+  double lambda, mu, rho;
+};
 
 class EasiMeshSize {
+  private:
+  easi::YAMLParser* parser;
+  easi::Component* model; // Unique ptr to model leads to segfault
+  std::string easiFileName;
   public:
-  EasiMeshSize(std::string_view easiFileName) {
-    // TODO(Lukas) Do something
-  }
+  EasiMeshSize();;
+  EasiMeshSize(std::string easiFileName);
 
-  double getMeshSize(std::array<double, 3> point) {
-    if(point[2] > 0 ) {
-      return 1.0;
-    } else {
-      return 0.1;
-    }
-  }
+  double getMeshSize(std::array<double, 3> point);
+
+  ~EasiMeshSize();
 };
 
 #endif // PUMGEN_EASIMESHSIZE_H
